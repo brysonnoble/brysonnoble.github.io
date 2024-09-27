fb62e02d54f95fa31afef1de7d6344178c832eaf
Bryson Noble <noblebryson@gmail.com>
2024-09-27 17:24:28 -0400

    Update retrieve.js

--- Programming Projects/StudentSpeak/retrieve.js
+++ Programming Projects/StudentSpeak/retrieve.js
@@ -1,4 +1,6 @@
 function Generate (resNum) {
+  document.getElementById("output").innerHTML = "<i>Generating...</i>";
+  
   for (let i = 0; i < resNum; i++) {
     document.getElementById("output").innerHTML += "<br><br>" + samples[Math.floor(Math.random()*samples.length)];
   }
