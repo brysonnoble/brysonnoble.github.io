089593f31fd9865490cd5ff98fe2b58c2aea05ba
Bryson Noble <noblebryson@gmail.com>
2024-09-28 17:17:01 -0400

    Update sync.js

--- Programming Projects/SmartSync/sync.js
+++ Programming Projects/SmartSync/sync.js
@@ -1,13 +1,16 @@
 function LoadPlayer (file) {
   switch (parseInt(file)) {
     case 0:
-      document.getElementById("player").src = "https://github.com/brysonnoble/brysonnoble.github.io/blob/main/Programming%20Projects/SmartSync/Sample_Audio/David%20Guetta%20%26%20OneRepublic%20-%20I%20Don't%20Wanna%20Wait.mp3";
+      var audio = new Audio('https://github.com/brysonnoble/brysonnoble.github.io/blob/main/Programming%20Projects/SmartSync/Sample_Audio/a0.mp3');
+      audio.play();
       break;
     case 1:
-      document.getElementById("player").src = "https://github.com/brysonnoble/brysonnoble.github.io/blob/main/Programming%20Projects/SmartSync/Sample_Audio/Artemas%20-%20i%20like%20the%20way%20you%20kiss%20me.mp3";
+      var audio = new Audio('https://github.com/brysonnoble/brysonnoble.github.io/blob/main/Programming%20Projects/SmartSync/Sample_Audio/a1.mp3');
+      audio.play();
       break;
     case 2:
-      document.getElementById("player").src = "https://github.com/brysonnoble/brysonnoble.github.io/blob/main/Programming%20Projects/SmartSync/Sample_Audio/Benson%20Boone%20-%20Slow%20It%20Down.mp3";
+      var audio = new Audio('https://github.com/brysonnoble/brysonnoble.github.io/blob/main/Programming%20Projects/SmartSync/Sample_Audio/a2.mp3');
+      audio.play();
       break;
     default:
       break;
