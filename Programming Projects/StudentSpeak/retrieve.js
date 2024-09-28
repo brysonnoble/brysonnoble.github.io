c6a7487adc185856bab5ac0f4c0a3fd2cb0732ef
Bryson Noble <noblebryson@gmail.com>
2024-09-27 22:20:44 -0400

    Update retrieve.js

--- Programming Projects/StudentSpeak/retrieve.js
+++ Programming Projects/StudentSpeak/retrieve.js
@@ -1,19 +1,17 @@
 function Generate(resNum) {
   for (let i = 0; i < resNum; i++) {
+    const newTextElement = document.createElement('span');
+    newTextElement.textContent = samples[Math.floor(Math.random() * samples.length)];
+
+    newTextElement.style.opacity = 0;
+    newTextElement.style.transition = 'opacity 0.5s ease-in-out';
+
+    document.getElementById("outputText").appendChild(newTextElement);
+    document.getElementById("outputText").appendChild(document.createElement('br'));
+    document.getElementById("outputText").appendChild(document.createElement('br'));
+
     setTimeout(() => {
-      const newTextElement = document.createElement('span');
-      newTextElement.textContent = samples[Math.floor(Math.random() * samples.length)];
-  
-      newTextElement.style.opacity = 0;
-      newTextElement.style.transition = 'opacity 0.5s ease-in-out';
-  
-      document.getElementById("outputText").appendChild(newTextElement);
-      document.getElementById("outputText").appendChild(document.createElement('br'));
-      document.getElementById("outputText").appendChild(document.createElement('br'));
-  
-      setTimeout(() => {
-        newTextElement.style.opacity = 1;
-      }, 10);
+      newTextElement.style.opacity = 1;
     }, 10);
   }
 }
