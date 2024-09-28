ee9aa60ea1b503b2ac1288dd8b1ff923ef394580
Bryson Noble <noblebryson@gmail.com>
2024-09-27 21:41:29 -0400

    Create visibility.js

---
+++ Programming Projects/StudentSpeak/visibility.js
@@ -0,0 +1,8 @@
+function toggleVis() {
+  var element = document.getElementById("explanationWindow");
+  if (element.style.display === "none") {
+    element.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
