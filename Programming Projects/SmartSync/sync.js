f6e6e989efbc937f0b5ae74e356eccd92e76d15f
Bryson Noble <noblebryson@gmail.com>
2024-09-28 16:29:49 -0400

    Update sync.js

--- Programming Projects/SmartSync/sync.js
+++ Programming Projects/SmartSync/sync.js
@@ -1,13 +1,13 @@
 function LoadPlayer (file) {
   switch (parseInt(file)) {
     case 0:
-      document.getElementById("player").src = "/Sample_Audio/David Guetta & OneRepublic - I Don't Wanna Wait.mp3";
+      document.getElementById("player").src = "https://github.com/brysonnoble/brysonnoble.github.io/blob/main/Programming%20Projects/SmartSync/Sample_Audio/David%20Guetta%20%26%20OneRepublic%20-%20I%20Don't%20Wanna%20Wait.mp3";
       break;
     case 1:
-      document.getElementById("player").src = "/Sample_Audio/Artemas - i like the way you kiss me.mp3";
+      document.getElementById("player").src = "https://github.com/brysonnoble/brysonnoble.github.io/blob/main/Programming%20Projects/SmartSync/Sample_Audio/Artemas%20-%20i%20like%20the%20way%20you%20kiss%20me.mp3";
       break;
     case 2:
-      document.getElementById("player").src = "/Sample_Audio/Benson Boone - Slow It Down.mp3";
+      document.getElementById("player").src = "https://github.com/brysonnoble/brysonnoble.github.io/blob/main/Programming%20Projects/SmartSync/Sample_Audio/Benson%20Boone%20-%20Slow%20It%20Down.mp3";
       break;
     default:
       break;
