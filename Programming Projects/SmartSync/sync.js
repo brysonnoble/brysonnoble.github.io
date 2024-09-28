359d83fdbe3134334918206648f3c1e00ef2448f
Bryson Noble <noblebryson@gmail.com>
2024-09-28 16:05:18 -0400

    Update sync.js

--- Programming Projects/SmartSync/sync.js
+++ Programming Projects/SmartSync/sync.js
@@ -1,13 +1,13 @@
 function LoadPlayer (file) {
-  switch (file) {
+  switch (parseInt(file)) {
     case 0:
-      document.getElementById("player").src = ;
+      document.getElementById("player").src = "/Sample_Audio/David Guetta & OneRepublic - I Don't Wanna Wait.mp3";
       break;
     case 1:
-      document.getElementById("player").src = ;
+      document.getElementById("player").src = "/Sample_Audio/Artemas - i like the way you kiss me.mp3";
       break;
     case 2:
-      document.getElementById("player").src = ;
+      document.getElementById("player").src = "/Sample_Audio/Benson Boone - Slow It Down.mp3";
       break;
     default:
       break;
