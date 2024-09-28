c51b141e56f3acbe1953ac8acee0e4f293e3e063
Bryson Noble <noblebryson@gmail.com>
2024-09-28 15:57:50 -0400

    Update sync.js

---
+++ Programming Projects/SmartSync/sync.js
@@ -0,0 +1,15 @@
+function LoadPlayer (file) {
+  switch (file) {
+    case 0:
+      document.getElementById("player").src = ;
+      break;
+    case 1:
+      document.getElementById("player").src = ;
+      break;
+    case 2:
+      document.getElementById("player").src = ;
+      break;
+    default:
+      break;
+  }
+}
