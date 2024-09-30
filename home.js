9ab225345a068ca1b93c0674da1863fb3a4a30f9
Bryson Noble <noblebryson@gmail.com>
2024-09-30 18:26:07 -0400

    Update home.js

--- home.js
+++ home.js
@@ -1,13 +1,1 @@
-$(document).ready(function() {
-  $(window).scroll(function() {
-    var distanceFromTop = $(document).scrollTop();
-    if (distanceFromTop >= $('#header').height())
-    {
-      $('#sticky').fadeIn(200).addClass('fixed');
-    }
-    else
-    {
-      $('#sticky').fadeOut(200).removeClass('fixed');
-    }
-  });
-});
+
