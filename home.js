4867d95209464c965cfd35a412bff359b23232df
Bryson Noble <noblebryson@gmail.com>
2024-09-30 19:20:42 -0400

    Update home.js

--- home.js
+++ home.js
@@ -1,3 +1,3 @@
 function loadSplash () {
-  document.getElementById("splash").style.backgroundImage = `url(${splashImages[Math.floor(Math.random() * splashImages.length)]})`;
+  document.getElementById("splash").style.backgroundImage = `url(${splashImages[Math.floor(Math.random() * splashImages.length)]}), linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)`;
 }
