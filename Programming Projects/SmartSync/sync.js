c2f28aebb05ea681065ddc09a817e8ce954a873f
Bryson Noble <noblebryson@gmail.com>
2024-09-29 22:04:47 -0400

    Update sync.js

--- Programming Projects/SmartSync/sync.js
+++ Programming Projects/SmartSync/sync.js
@@ -2,16 +2,16 @@ function PlaySample (file) {
   alert("test");
   switch (file) {
     case 0:
-      const audio = new Audio('https://brysonnoble.github.io/Programming%20Projects/SmartSync/Sample_Audio/a0.mp3');
-      audio.play();
+      const audio0 = new Audio('https://brysonnoble.github.io/Programming%20Projects/SmartSync/Sample_Audio/a0.mp3');
+      audio0.play();
       break;
     case 1:
-      const audio = new Audio('https://brysonnoble.github.io/Programming%20Projects/SmartSync/Sample_Audio/a1.mp3');
-      audio.play();
+      const audio1 = new Audio('https://brysonnoble.github.io/Programming%20Projects/SmartSync/Sample_Audio/a1.mp3');
+      audio1.play();
       break;
     case 2:
-      const audio = new Audio('https://brysonnoble.github.io/Programming%20Projects/SmartSync/Sample_Audio/a2.mp3');
-      audio.play();
+      const audio2 = new Audio('https://brysonnoble.github.io/Programming%20Projects/SmartSync/Sample_Audio/a2.mp3');
+      audio2.play();
       break;
     default:
       break;
