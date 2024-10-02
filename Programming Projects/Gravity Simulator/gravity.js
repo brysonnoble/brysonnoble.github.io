6871777ed632daa946578a2e88f69de9d2466b19
Bryson Noble <noblebryson@gmail.com>
2024-10-02 06:44:18 -0400

    Update gravity.js

--- Programming Projects/Gravity Simulator/gravity.js
+++ Programming Projects/Gravity Simulator/gravity.js
@@ -1,12 +1,51 @@
-// https://spark.iop.org/calculating-gravitational-forces
 let G = 6.67 * (10 ** -11);
+let circleIdCounter = 0;
+const circlesList = [];
+
+function createCircle(x, y) {
+  const circle = document.createElement('div');
+  circle.classList.add('circle');
+  
+  const circleId = `circle-${circleIdCounter++}`;
+  circle.setAttribute('id', circleId);
+  
+  circle.style.left = `${x - 10}px`;
+  circle.style.top = `${y - 10}px`;
+
+  circlesList.push({ id: circleId, x: x, y: y });
+  console.log(circlesList);
+
+  circle.addEventListener('click', function(e) {
+    e.stopPropagation();
+    document.body.removeChild(circle);
+    removeCircleFromList(circleId);
+  });
+
+  document.body.appendChild(circle);
+}
+
+function removeCircleFromList(circleId) {
+  const index = circlesList.findIndex(circle => circle.id === circleId);
+  if (index !== -1) {
+    circlesList.splice(index, 1);
+    console.log(circlesList);
+  }
+}
+
+document.addEventListener('click', function(event) {
+  const controlPanel = document.getElementById('controlPanel');
+
+  const isClickInsideControlPanel = controlPanel.contains(event.target);
+
+  if (!isClickInsideControlPanel) {
+    createCircle(event.clientX, event.clientY);
+  }
+});
 
 function Simulate (circlesList) {
   for (let i = 0; i < circlesList.length; i++) {
     for (let j = i + 1; j < circlesList.length; j++) {
-      //console.log(`Calculating distance between ${circlesList[i].id} and ${circlesList[j].id}:`, Distance(circlesList[i].x, circlesList[i].y, circlesList[j].x, circlesList[j].y));
-      //console.log(`Calculating force between ${circlesList[i].id} and ${circlesList[j].id}:`, Force(1, 1, Distance(circlesList[i].x, circlesList[i].y, circlesList[j].x, circlesList[j].y)));
-      console.log(`Calculating angle between ${circlesList[i].id} and ${circlesList[j].id}:`, Direction(circlesList[i].x, circlesList[i].y, circlesList[j].x, circlesList[j].y));
+      Move(circlesList[i].id, circlesList[i].x, circlesList[i].y, circlesList[j].id, circlesList[j].x, circlesList[j].y, Force(1, 1, Distance(circlesList[i].x, circlesList[i].y, circlesList[j].x, circlesList[j].y))), Direction(circlesList[i].x, circlesList[i].y, circlesList[j].x, circlesList[j].y));
     }
   }
 }
@@ -23,7 +62,6 @@ function Direction (x1, y1, x2, y2) {
   return (Math.atan((x2 - x1) / (y2 - y1))) * (180 / Math.PI);
 }
 
-function Move () {
+function Move (c1, x1, y1, c2, x2, y2, force, direction) {
   
 }
-
