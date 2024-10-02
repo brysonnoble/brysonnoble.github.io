19eba3cae624fba2c6bdbe187711fd5ac4dfb2df
Bryson Noble <noblebryson@gmail.com>
2024-10-02 13:13:32 -0400

    Update gravity.js

--- Programming Projects/Gravity Simulator/gravity.js
+++ Programming Projects/Gravity Simulator/gravity.js
@@ -78,17 +78,14 @@ function Distance (x1, y1, x2, y2) {
   return (((x2 - x1) ** 2) + ((y2 - y1) ** 2)) ** 0.5;
 }
 
-function Force (m1, m2, dist) {
-  const MIN_DIST = 10;
+function Force(m1, m2, dist) {
+  const MIN_DIST = 10; // Avoid forces going to infinity
   if (dist < MIN_DIST) dist = MIN_DIST;
   return ((1000) * m1 * m2) / (dist ** 2);
 }
 
 function Direction (x1, y1, x2, y2) {
-  const dx = x2 - x1;
-  const dy = y2 - y1;
-  const magnitude = Math.sqrt(dx * dx + dy * dy);
-  return [dx / magnitude, dy / magnitude];
+  return [(x2 - x1), (y2 - y1)];
 }
 
 function Vector2Translate (p, force, direction) {
