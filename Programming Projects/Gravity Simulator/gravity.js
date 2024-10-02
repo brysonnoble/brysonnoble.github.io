541290f1c70b70da3d3c638af6327627a75f95c4
Bryson Noble <noblebryson@gmail.com>
2024-10-01 23:55:57 -0400

    Update gravity.js

--- Programming Projects/Gravity Simulator/gravity.js
+++ Programming Projects/Gravity Simulator/gravity.js
@@ -1,1 +1,12 @@
 // https://spark.iop.org/calculating-gravitational-forces
+let G = 6.67 * (10 ** -11);
+
+function Simulate () {
+  for (let i = 0; i < circleIdCounter; i++) {
+    
+  }
+}
+
+function Distance (x1, y1, x2, y2) {
+  return (((x2 - x1) ** 2) + ((y2 - y1) ** 2)) ** 0.5;
+}
