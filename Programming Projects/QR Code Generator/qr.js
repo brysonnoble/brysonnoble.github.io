55cc9e9411d5cfff5c8f7514dd5d7db6a42f8e93
Bryson Noble <noblebryson@gmail.com>
2024-12-18 23:26:45 -0400

    Update qr.js

--- Programming Projects/QR Code Generator/qr.js
+++ Programming Projects/QR Code Generator/qr.js
@@ -100,7 +100,7 @@ function generateCharCount (version, charCount) {
 }
 
 // add leading zeros
-function pad(n, width, z) {
+function pad (n, width, z) {
   z = z || '0';
   n = n + '';
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
