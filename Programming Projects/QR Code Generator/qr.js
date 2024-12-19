44eb44f330dcb3ce8252737c58c46a169dde97e7
Bryson Noble <noblebryson@gmail.com>
2024-12-18 23:06:54 -0400

    Update qr.js

--- Programming Projects/QR Code Generator/qr.js
+++ Programming Projects/QR Code Generator/qr.js
@@ -4,85 +4,87 @@
 
 // change between versions depending on character count
 function versionCheck () {
-  if (document.getElementById("QRString").value.length <= 17) {
-    alert(charCount(1));
-  } else if (document.getElementById("QRString").value.length <= 32) {
+  const charCount = document.getElementById("QRString").value.length;
+  
+  if (charCount <= 17) {
+    alert(generateCharCount(1, charCount));
+  } else if (charCount <= 32) {
     alert('version 2');
-  } else if (document.getElementById("QRString").value.length <= 53) {
+  } else if (charCount <= 53) {
     alert('version 3');
-  } else if (document.getElementById("QRString").value.length <= 78) {
+  } else if (charCount <= 78) {
     alert('version 4');
-  } else if (document.getElementById("QRString").value.length <= 106) {
+  } else if (charCount <= 106) {
     alert('version 5');
-  } else if (document.getElementById("QRString").value.length <= 134) {
+  } else if (charCount <= 134) {
     alert('version 6');
-  } else if (document.getElementById("QRString").value.length <= 154) {
+  } else if (charCount <= 154) {
     alert('version 7');
-  } else if (document.getElementById("QRString").value.length <= 192) {
+  } else if (charCount <= 192) {
     alert('version 8');
-  } else if (document.getElementById("QRString").value.length <= 230) {
+  } else if (charCount <= 230) {
     alert('version 9');
-  } else if (document.getElementById("QRString").value.length <= 271) {
+  } else if (charCount <= 271) {
     alert('version 10');
-  } else if (document.getElementById("QRString").value.length <= 321) {
+  } else if (charCount <= 321) {
     alert('version 11');
-  } else if (document.getElementById("QRString").value.length <= 367) {
+  } else if (charCount <= 367) {
     alert('version 12');
-  } else if (document.getElementById("QRString").value.length <= 425) {
+  } else if (charCount <= 425) {
     alert('version 13');
-  } else if (document.getElementById("QRString").value.length <= 458) {
+  } else if (charCount <= 458) {
     alert('version 14');
-  } else if (document.getElementById("QRString").value.length <= 520) {
+  } else if (charCount <= 520) {
     alert('version 15');
-  } else if (document.getElementById("QRString").value.length <= 586) {
+  } else if (charCount <= 586) {
     alert('version 16');
-  } else if (document.getElementById("QRString").value.length <= 644) {
+  } else if (charCount <= 644) {
     alert('version 17');
-  } else if (document.getElementById("QRString").value.length <= 718) {
+  } else if (charCount <= 718) {
     alert('version 18');
-  } else if (document.getElementById("QRString").value.length <= 792) {
+  } else if (charCount <= 792) {
     alert('version 19');
-  } else if (document.getElementById("QRString").value.length <= 858) {
+  } else if (charCount <= 858) {
     alert('version 20');
-  } else if (document.getElementById("QRString").value.length <= 929) {
+  } else if (charCount <= 929) {
     alert('version 21');
-  } else if (document.getElementById("QRString").value.length <= 1003) {
+  } else if (charCount <= 1003) {
     alert('version 22');
-  } else if (document.getElementById("QRString").value.length <= 1091) {
+  } else if (charCount <= 1091) {
     alert('version 23');
-  } else if (document.getElementById("QRString").value.length <= 1171) {
+  } else if (charCount <= 1171) {
     alert('version 24');
-  } else if (document.getElementById("QRString").value.length <= 1273) {
+  } else if (charCount <= 1273) {
     alert('version 25');
-  } else if (document.getElementById("QRString").value.length <= 1367) {
+  } else if (charCount <= 1367) {
     alert('version 26');
-  } else if (document.getElementById("QRString").value.length <= 1465) {
+  } else if (charCount <= 1465) {
     alert('version 27');
-  } else if (document.getElementById("QRString").value.length <= 1528) {
+  } else if (charCount <= 1528) {
     alert('version 28');
-  } else if (document.getElementById("QRString").value.length <= 1628) {
+  } else if (charCount <= 1628) {
     alert('version 29');
-  } else if (document.getElementById("QRString").value.length <= 1732) {
+  } else if (charCount <= 1732) {
     alert('version 30');
-  } else if (document.getElementById("QRString").value.length <= 1840) {
+  } else if (charCount <= 1840) {
     alert('version 31');
-  } else if (document.getElementById("QRString").value.length <= 1952) {
+  } else if (charCount <= 1952) {
     alert('version 32');
-  } else if (document.getElementById("QRString").value.length <= 2068) {
+  } else if (charCount <= 2068) {
     alert('version 33');
-  } else if (document.getElementById("QRString").value.length <= 2188) {
+  } else if (charCount <= 2188) {
     alert('version 34');
-  } else if (document.getElementById("QRString").value.length <= 2303) {
+  } else if (charCount <= 2303) {
     alert('version 35');
-  } else if (document.getElementById("QRString").value.length <= 2431) {
+  } else if (charCount <= 2431) {
     alert('version 36');
-  } else if (document.getElementById("QRString").value.length <= 2563) {
+  } else if (charCount <= 2563) {
     alert('version 37');
-  } else if (document.getElementById("QRString").value.length <= 2699) {
+  } else if (charCount <= 2699) {
     alert('version 38');
-  } else if (document.getElementById("QRString").value.length <= 2809) {
+  } else if (charCount <= 2809) {
     alert('version 39');
-  } else if (document.getElementById("QRString").value.length <= 2953) {
+  } else if (charCount <= 2953) {
     alert('version 40');
   } else {
     alert('invalid number of characters');
@@ -90,6 +92,6 @@ function versionCheck () {
 }
 
 // generate binary character count
-function charCount (const version) {
+function generateCharCount (version, charCount) {
   return version;
 }
