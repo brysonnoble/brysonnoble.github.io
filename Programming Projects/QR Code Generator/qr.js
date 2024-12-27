d3b8750a2a5516caf5877723056188ee3036ee2f
Bryson Noble <noblebryson@gmail.com>
2024-12-26 21:08:18 -0500

    Update qr.js

--- Programming Projects/QR Code Generator/qr.js
+++ Programming Projects/QR Code Generator/qr.js
@@ -319,7 +319,7 @@ function alignmentPatterns (matrix, V) {
 
   for (let i = 0; i < 5; i++) {
     for (let j = 0; j < 5; j++) {
-      for (let k = 0; k < locations[V].length - 1; k++) {
+      for (let k = 0; k < locations[V].length; k++) {
         matrix[i + locations[V][k]][j + locations[V][k]] = pattern[i][j];
       }
     }
