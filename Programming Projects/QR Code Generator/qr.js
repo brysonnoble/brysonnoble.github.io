a45e444498252f5080a5f8a24e5afb040a8b347a
Bryson Noble <noblebryson@gmail.com>
2024-12-26 20:16:33 -0500

    Update qr.js

--- Programming Projects/QR Code Generator/qr.js
+++ Programming Projects/QR Code Generator/qr.js
@@ -314,7 +314,7 @@ function reservedAreas (matrix, V) {
     for (let i = 0; i < 8; i++) {
       matrix[i][8] = 2;
       matrix[8][i] = 2;
-    //  matrix[i + (((V-1)*4)+21) - 9][(((V-1)*4)+21) - 9] = 2;
+      matrix[i + (((V-1)*4)+21) - 9][8] = 2;
     }
   }
   
