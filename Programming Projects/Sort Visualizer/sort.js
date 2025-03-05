dee4343f922573f2652cc789c1adcb5b067e5a2b
Bryson Noble <noblebryson@gmail.com>
2025-03-05 14:51:59 -0500

    Update sort.js

--- Programming Projects/Sort Visualizer/sort.js
+++ Programming Projects/Sort Visualizer/sort.js
@@ -55,3 +55,21 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function selectionSort(arr) {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    
+    let min_idx = i;
+        
+    for (let j = i + 1; j < n; j++) {
+        if (arr[j] < arr[min_idx]) {
+            min_idx = j;
+        }
+    }
+        
+    let temp = arr[i];
+    arr[i] = arr[min_idx];
+    arr[min_idx] = temp;
+  }
+}
