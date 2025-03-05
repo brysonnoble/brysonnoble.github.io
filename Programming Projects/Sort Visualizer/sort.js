b9a8f6169750c19ab5f9c7a3428a4bdc2a3c6fa5
Bryson Noble <noblebryson@gmail.com>
2025-03-05 13:30:08 -0500

    Update sort.js

--- Programming Projects/Sort Visualizer/sort.js
+++ Programming Projects/Sort Visualizer/sort.js
@@ -1,1 +1,56 @@
+function randomizeAll(n) {
+  const sortIds = [
+    "selectionSort",
+    "bubbleSort",
+    "insertionSort",
+    "mergeSort",
+    "quickSort",
+    "heapSort",
+    "cycleSort",
+    "3WayMergeSort",
+    "countingSort",
+    "radixSort",
+    "bucketSort",
+    "timSort",
+    "combSort",
+    "pigeonholeSort",
+    "introSort",
+    "timSortHybrid"
+  ];
 
+  sortIds.forEach(id => randomize(id, n));
+}
+
+function randomize(id, n) {
+  const container = document.getElementById(id);
+  if (!container) {
+    console.error(`Element with id '${id}' not found.`);
+    return;
+  }
+
+  const display = container.querySelector(".display");
+  if (!display) {
+    console.error(`Display div not found for '${id}'.`);
+    return;
+  }
+
+  display.innerHTML = "";
+
+  for (let i = 0; i < n; i++) {
+    const bar = document.createElement('div');
+    bar.style.width = `${320 / n}px`;
+    bar.style.height = `${Math.random() * 160}px`;
+    bar.style.background = "red";
+    bar.style.display = "inline-block";
+    bar.style.margin = "1px";
+
+    display.appendChild(bar);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const randomizeAllBtn = document.querySelector("input[value='Randomize All']");
+  if (randomizeAllBtn) {
+    randomizeAllBtn.addEventListener("click", () => randomizeAll(20)); // Default 20 elements
+  }
+});
