d9bfb769da1999419ebd91e0fd3548454cddbf71
Bryson Noble <noblebryson@gmail.com>
2025-03-25 19:02:10 -0400

    Update box.js

--- Programming Projects/Lockbox/box.js
+++ Programming Projects/Lockbox/box.js
@@ -1,1 +1,10 @@
+function encrypt() {
+  const key = document.getElementById("key").value;
+  const passwords = document.getElementById("passwords").value;
 
+  // document.getElementById("passwords").value = whatever
+}
+
+function decrypt() {
+
+}
