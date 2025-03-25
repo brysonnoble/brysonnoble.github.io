6222b1a0d35c9984b42de2f9a6c351e0c0c17776
Bryson Noble <noblebryson@gmail.com>
2025-03-25 13:07:41 -0400

    Update terminal.js

--- Programming Projects/zsh/terminal.js
+++ Programming Projects/zsh/terminal.js
@@ -1,11 +1,13 @@
-const textarea = document.getElementById('terminal');
-
-textarea.value = '~/ > ';
-
-textarea.addEventListener('keydown', (event) => {
-    if (event.key === 'Enter') {
-        event.preventDefault();
-        const currentText = textarea.value;
-        textarea.value = currentText + '\n~/ > ';
-    }
+document.addEventListener('DOMContentLoaded', () => {
+    const textarea = document.getElementById('terminal');
+    
+    textarea.value = '~/ > ';
+    
+    textarea.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            const currentText = textarea.value;
+            textarea.value = currentText + '\n~/ > ';
+        }
+    });
 });
