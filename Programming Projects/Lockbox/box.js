38f7cc7f5c731ff8ad2d21bcbb92e8f268d47108
Bryson Noble <noblebryson@gmail.com>
2025-03-25 23:09:20 -0400

    Update box.js

--- Programming Projects/Lockbox/box.js
+++ Programming Projects/Lockbox/box.js
@@ -1,13 +1,36 @@
-function encrypt() {
+function lock () {
   const key = document.getElementById("key").value;
-  var passwords = document.getElementById("passwords").value.replace(/\r\n/g,"\n").split("\n");
+  const passwords = document.getElementById("passwords").value.replace(/\r\n/g,"\n").split("\n");
+  const output = [];
 
-  alert(key);
-  alert(passwords);
+  console.log(key);
+  console.log(passwords);
 
+  for (const e of passwords) {
+    output.push(encrypt(e, key));
+  }
+
+  console.log(output);
+  
   // document.getElementById("passwords").value = whatever
 }
 
-function decrypt() {
+function encrypt (password, key) {
+  pAscii = []
+  for (const c of password) {
+    pAscii.push(c.charCodeAt(0));
+  }
+  console.log(pAscii);
+  
+  kAscii = []
+  for (const c of key) {
+    kAscii.push(key.charCodeAt(0));
+  }
+  console.log(kAscii);
+
+  return "hi";
+}
+
+function unlock () {
 
 }
