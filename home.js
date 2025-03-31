a6ce36f7aca7cb76433fd0ea9b6eb9a961ad51a0
Bryson Noble <noblebryson@gmail.com>
2025-03-30 22:28:54 -0400

    Update home.js

--- home.js
+++ home.js
@@ -11,6 +11,6 @@ function dates () {
   for (let i = 0; i < document.getElementsByClassName("date").length; i++) {
     const date = new Date();
     const year = date.getFullYear();
-    document.getElementsByClassName("date")[i].textContent = year;
+    document.getElementsByClassName("date")[i].textContent = year - document.getElementsByClassName("date")[i].getAttribute('subtractTime');
   }
 }
