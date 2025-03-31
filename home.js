593c0e35a3b2be085fb4c090e9ffe933a9c399d1
Bryson Noble <noblebryson@gmail.com>
2025-03-30 22:10:50 -0400

    Update home.js

--- home.js
+++ home.js
@@ -1,3 +1,11 @@
 function loadSplash () {
   document.getElementById("splash").style.backgroundImage = `url(${splashImages[Math.floor(Math.random() * splashImages.length)]}), linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)`;
 }
+
+function dates () {
+  for (let i = 0; i < document.getElementsByClassName("date").length; i++) {
+    const date = new Date();
+    const year = date.getFullYear();
+    document.getElementsByClassName("date")[i].textContent = year;
+  }
+}
