3b5c90c74c22561635655c843e67833a9ff6580c
Bryson Noble <noblebryson@gmail.com>
2025-04-03 18:18:49 -0400

    Update home.js

--- home.js
+++ home.js
@@ -1,22 +1,32 @@
 window.addEventListener('load', function() {
   loadSplash();
   dates();
-  hideLoader();
 });
 
-function loadSplash () {
-  document.getElementById("splash").style.backgroundImage = `url(${splashImages[Math.floor(Math.random() * splashImages.length)]}), linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)`;
+function loadSplash() {
+  const splashElement = document.getElementById("splash");
+  const image = new Image();
+  const randomSplash = splashImages[Math.floor(Math.random() * splashImages.length)];
+  
+  image.src = randomSplash; // Set the image source
+  
+  image.onload = function() {
+    splashElement.style.backgroundImage = `url(${randomSplash}), linear-gradient(rgba(0,0,0,0.5),rgba(0,0,0,0.5)`;
+    hideLoader();
+  };
 }
 
-function dates () {
-  for (let i = 0; i < document.getElementsByClassName("date").length; i++) {
-    const date = new Date();
-    const year = date.getFullYear();
-    document.getElementsByClassName("date")[i].textContent = year - document.getElementsByClassName("date")[i].getAttribute('subtractTime');
+function dates() {
+  const dateElements = document.getElementsByClassName("date");
+  const currentYear = new Date().getFullYear();
+
+  for (let i = 0; i < dateElements.length; i++) {
+    const subtractTime = dateElements[i].getAttribute('subtractTime');
+    dateElements[i].textContent = currentYear - subtractTime;
   }
 }
 
-function hideLoader () {
+function hideLoader() {
   document.getElementById("load").style.display = "none";
   document.body.style.overflow = 'scroll';
 }
