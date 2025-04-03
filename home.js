0cebb2db0eed559ab834ca4e7a2d0e59b11b244c
Bryson Noble <noblebryson@gmail.com>
2025-04-03 18:28:17 -0400

    Update home.js

--- home.js
+++ home.js
@@ -41,6 +41,7 @@ function hideLoader() {
       document.body.style.overflow = 'scroll';
       
       h2.style.animation = "typing 3.5s steps(40, end), blink-caret 0.75s step-end 5";
+      h2.style.color = "white";
     }
   }, 50);
 }
