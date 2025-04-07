bd5b2e0f73b4b97bc28fa7cb7ee3f69bbf02ebba
Bryson Noble <noblebryson@gmail.com>
2025-04-06 22:38:15 -0400

    parking planner initial

---
+++ Programming Projects/Parking Planner/theme.js
@@ -0,0 +1,18 @@
+let darkmode = localStorage.getItem('darkmode');
+
+const enableDarkmode = () => {
+  document.body.classList.add('darkmode');
+  localStorage.setItem('darkmode', 'active');
+}
+
+const disableDarkmode = () => {
+  document.body.classList.remove('darkmode');
+  localStorage.setItem('darkmode', null);
+}
+
+if(darkmode === "active") enableDarkmode();
+
+function themeSwitch () {
+  darkmode = localStorage.getItem('darkmode');
+  darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+}
