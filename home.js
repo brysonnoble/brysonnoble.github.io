e2e35dc8d00f64f2a5a38c6eac5d0224bbb92e09
Bryson Noble <noblebryson@gmail.com>
2025-04-17 06:52:51 -0400

    Update home.js

--- home.js
+++ home.js
@@ -35,7 +35,7 @@ function hideLoader () {
   const fadeOut = setInterval(() => {
     document.body.style.overflow = 'scroll';
 
-    h2.style.animation = "typing 3.5s steps(40, end), blink-caret 0.75s step-end 5";
+    h2.style.animation = "typing 2s steps(40, end), blink-caret 0.75s step-end 4";
     h2.style.color = "white";
     setTimeout(enableWrap, 4250);
     
