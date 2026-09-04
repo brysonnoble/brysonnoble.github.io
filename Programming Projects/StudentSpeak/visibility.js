1a287c550f7cc23f940475e659754c667413ef0b
brysonnoble <noblebryson@gmail.com>
2026-09-03 22:17:14 -0400

    Move SmartSync and StudentSpeak pages to their own repos
    Both pages now live on the gh-pages branch of their respective
    repos (brysonnoble/SmartSync, brysonnoble/StudentSpeak) and are
    served via GitHub Pages. Updated links in index.htm accordingly.

--- Programming Projects/StudentSpeak/visibility.js
+++
@@ -1,8 +0,0 @@
-function toggleVis () {
-  var element = document.getElementById("explanationWindow");
-  if (element.style.display === "none") {
-    element.style.display = "block";
-  } else {
-    element.style.display = "none";
-  }
-}
